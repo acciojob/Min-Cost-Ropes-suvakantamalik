fd5d43f8198111a6d717282c9a4138d0a43ae34f
suvakantamalik <134865642+suvakantamalik@users.noreply.github.com>
2024-08-17 00:32:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,24 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+  let pq = [];
+	for(let i=0; i<arr.length; i++){
+		pq.push(arr[i]);
+	}
+	
+	pq.sort(function(a,b){
+		return a-b;
+	});
+	
+	let res = 0;
+	while(pq.length > 1){
+		let first = pq.shift();
+		let second = pq.shift();
+
+		res += (first + second);
+		pq.push(first + second);
+	}
+	return res;
 }
 
 module.exports=mincost;
